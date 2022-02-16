496256966aaa2d403f5c65d404d237b49252478d
thelastandrew <thelastandrew@gmail.com>
2022-02-16 06:08:29 +0300

    feat: add flip card conditions and function

--- memory-game/index.js
+++ memory-game/index.js
@@ -1,8 +1,39 @@
 const cards = document.querySelectorAll('.memory-card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
 //Flip card
 function flipCard() {
-  this.classList.toggle('flip');
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+  secondCard = this;
+  hasFlippedCard = false;
+  checkForMatch();
+}
+
+function checkForMatch() {
+  if (firstCard.dataset.framework === secondCard.dataset.framework) {
+    disableCards();
+    return;
+  }
+  unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+  }, 1200);
 }
 
 cards.forEach((card) => card.addEventListener('click', flipCard));
